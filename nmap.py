9b17d6f840f11e48d49234c99ddd2f20c1d665a7
Ashwinhacker <102268450+Ashwinhacker@users.noreply.github.com>
2023-09-11 20:16:59 +0530

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -1,4 +1,19 @@
+print("""                                                           
+888b      88                                               
+8888b     88                                               
+88 `8b    88                                               
+88  `8b   88  88,dPYba,,adPYba,   ,adPPYYba,  8b,dPPYba,   
+88   `8b  88  88P'   "88"    "8a  ""     `Y8  88P'    "8a  
+88    `8b 88  88      88      88  ,adPPPPP88  88       d8  
+88     `8888  88      88      88  88,    ,88  88b,   ,a8"  
+88      `888  88      88      88  `"8bbdP"Y8  88`YbbdP"'   
+                                              88           
+                                              88           
+
+""")
+
 import subprocess
+
 
 def run_nmap_scan():
     target = input("Enter the target IP address or hostname: ")
